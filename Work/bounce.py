4668a3dbcfdd6cb75ca7011b3914a3199e8695c2
Vishwajeet Kumar <228141986+vishwajeet-kumr@users.noreply.github.com>
2026-09-15 17:52:19 +0530

    completed basics

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height_of_tower = 100
+count_bounces = 0
+bounces_till = 3/5
+final_result = bounces_till * height_of_tower
+
+while  count_bounces < 10:
+    print(round(final_result, 4))
+    final_result = final_result*bounces_till
+    count_bounces += 1
